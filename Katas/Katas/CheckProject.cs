3b323880977d94114360cb977bae23f21ee72d61
Steven Treske <steven.treske@hotmail.com>
2018-06-09 22:35:52 +0200

    Initiale Solutionstruktur

---
+++ Katas/Katas/CheckProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Katas
+{
+    public class CheckProject
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
